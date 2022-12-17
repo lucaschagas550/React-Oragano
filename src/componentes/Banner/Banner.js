e85bbe1a2a47a5751acf0526609bc140bdda3389
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-12-17 16:25:06 -0300

    feat: banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img
+        src="/imagens/banner.png"
+        alt="O banner principal da pagina do Oragano"
+      />
+    </header>
+  );
+}
+
+export default Banner;
